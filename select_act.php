336a7f70598309f67b5dd5294a1e49e8b4c183e1
Hirohisa2017 <74123329+Hirohisa2017@users.noreply.github.com>
2021-04-06 00:39:34 +0900

    Add files via upload

---
+++ select_act.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+$preference = $_POST[preference];
+
+foreach($preference as $value){
+  echo '検索キーワード：'.$value."\n";
+}
+
+// var_dump($preference[0]);
+
+require_once('./message/funcs.php');
+
+//1. 接続します
+$pdo = db_conn();
+
+//２．データ登録SQL作成
+$sql="SELECT * FROM gs_user_table WHERE alumni_flag=1 and keywords LIKE '%$value%'";
+$stmt = $pdo->prepare($sql);
+$res = $stmt->execute();
+
+
+//SQL実行時にエラーがある場合
+$view="";
+if($res==false){
+  $error = $stmt->errorInfo();
+  exit("QueryError:".$error[2]);
+} else {
+  while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $view .="<p>";
+    $view .='<a href="./message/message.php?id='.$result["id"].'">';
+    $view .=$result["id"].':'.$result["name"];
+    $view .='</a>';
+    $view .="</p>";
+
+  }
+  print($view);
+    //実験hiddenでpostする実験
+      // $view .="<p>";
+      // $view .=$val["id"].':'.$val["alumni_name"];
+      // $view .="<a href='' onclick='document.form1.submit();return false;>";
+      // $view .="ダイレクトメッセージを送る";
+      // $view .="</a>";
+      // $view .="<form name='form1' method='POST' action='./mini_bbs/index.php'>";
+      // $view .="<input type='hidden' name='id' value='";
+      // $view .=$val['id'];
+      // $view .="'>";
+      // $view .="</form>";
+      // $view .="</p>";
+    }
+
+
+
+//３．抽出データ数を取得
+//$count = $stmt->fetchColumn(); //SELECT COUNT(*)で使用可能()
+// $val = $stmt->fetch(); //1レコードだけ取得する方法
+
+//４. 該当レコードがあればSESSIONに値を代入
+
+//処理終了
+exit();
+?>
